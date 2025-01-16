7747a54c7ce31a2d55cf4b282309a95969edac70
DavideSaia <davidesaia2701@gmail.com>
2025-01-16 16:45:09 +0100

    Add report

--- VI_Odometry.m
+++ VI_Odometry.m
@@ -14,17 +14,9 @@ clearvars
 %sampleRate as fuction of scenario sampleTime
 sampleRate = 1/scenario.SampleTime;  
                                       
-
 %Create Fusion Filter 
 filter = insfilterErrorState("IMUSampleRate",sampleRate,...
                              'ReferenceFrame','ENU');
-
-%Create sensors mounted on vehicle 
-[paramsVO,imu, mountingLocationIMU,...
-    mountingAnglesIMU] = createSensors(scenario);
-
-% Flag to choose if use VIO or only IMU
-useVIO = true;
 
 
 %Visualize the scene -------------------------------------------
@@ -43,26 +35,30 @@ hAxes2 = axes('Parent',hPanel2);
 plot(scenario,"RoadCenters","on","Waypoints","on",'Parent',hAxes1);
 chasePlot(gTCar,'Centerline','on','Parent',hAxes2,'Meshes','on');
 
-
-
 %--------------------------------------------------------------------------
 
 
+%SIMULATION SET UP USING SMOOTH TRAJECTORY --------------------------------
 
-%SIMULATION SET UP FIRST METHODS -----------------------------------------
+%Get the ground truth state
 groundTruth = state(gTCar);
 
 
+%Create sensors mounted on vehicle 
+[paramsVO,imu, mountingLocationIMU,...
+    mountingAnglesIMU] = createSensors(scenario);
+
+
 %Set the initial state and error state covariance 
-filterIntializer(filter,groundTruth,1);
+filterIntializer(filter,groundTruth,'1');
 
 % Convert orientation offset from Euler angles to quaternion.
 orientVeh2IMU = quaternion(mountingAnglesIMU,'eulerd','ZYX','frame');
 
 %Samples per camera 
 imuSamplesPerCamera = 4;
-numIMUSamples = 3368;
-numCameraSamples = 3368/4;
+numIMUSamples = 3368;%get from a dummy simulation
+numCameraSamples = numIMUSamples/4;
 
 % Preallocate data arrays for plotting results.
 [pos, orient, vel, acc, angvel, ...
@@ -79,8 +75,11 @@ i=1;%loop
 cameraIdx = 1;%camera sample 
 
 
+%Click a button to start
+pause()
 while advance(scenario)
     groundTruth = state(gTCar);
+
 
     % Unpack the ground truth struct by converting the orientations from
     % Euler angles to quaternions and converting angular velocities form
@@ -97,17 +96,19 @@ while advance(scenario)
     [posIMU,orientIMU,velIMU,accIMU,angvelIMU] = transformMotion( ...
         mountingLocationIMU,orientVeh2IMU, ...
         pos(i,:),orient(i),vel(i,:),acc(i,:),angvel(i,:));
-    
+
+
     % Generate accelerometer and gyroscope measurements from the 
     % ground truth trajectory values.
     [accel, gyro] = imu(accIMU,angvelIMU,orientIMU);
+
 
     %Predict the filter state forward one time step based on the
     % accelerometer and gyroscope measurements.
     predict(filter, accel, gyro); 
 
 
-    if (1 == mod(i, imuSamplesPerCamera)) && useVIO
+    if (1 == mod(i, imuSamplesPerCamera)) 
         % Generate a visual odometry pose estimate from the ground truth
         % values and the visual odometry model.
         [posVO(cameraIdx,:), orientVO(cameraIdx,:), paramsVO] = ...
@@ -120,12 +121,13 @@ while advance(scenario)
         cameraIdx = cameraIdx + 1;
     end
 
+    %Get the pose estimated by the filter 
     [posEst(i,:), orientEst(i,:), velEst(i,:)] = pose(filter);
-
 
     % Update estimated vehicle pose.
     updatePose(estCar, posEst(i,:), velEst(i,:), orientEst(i));
 
+    % Update driving scenario visualization.
     updatePlots(scenario)
     drawnow limitrate;
 
@@ -135,19 +137,14 @@ end
 
 %PLOT RESULTS
 figure
-if useVIO
-    plot3(pos(:,1), pos(:,2), pos(:,3), '-.', ...
-        posVO(:,1), posVO(:,2), posVO(:,3), ...
-        posEst(:,1), posEst(:,2), posEst(:,3), ...
-        'LineWidth', 3)
-    legend('Ground Truth', 'Visual Odometry (VO)', ...
-        'Visual-Inertial Odometry (VIO)', 'Location', 'northeast')
-else
-    plot3(pos(:,1), pos(:,2), pos(:,3), '-.', ...
-        posEst(:,1), posEst(:,2), posEst(:,3), ...
-        'LineWidth', 3)
-    legend('Ground Truth', 'IMU Pose Estimate')
-end
+
+plot3(pos(:,1), pos(:,2), pos(:,3), '-.', ...
+    posVO(:,1), posVO(:,2), posVO(:,3), ...
+    posEst(:,1), posEst(:,2), posEst(:,3), ...
+    'LineWidth', 3)
+legend('Ground Truth', 'Visual Odometry (VO)', ...
+    'Visual-Inertial Odometry (VIO)', 'Location', 'northeast')
+
 view(-90, 90)
 title('Vehicle Position')
 xlabel('X (m)')
@@ -156,16 +153,22 @@ grid on
 
 %------------------------------------------------------------------------
 
+%Click a button to start
+pause();
+
+%SIMULATION SET UP USING WAYPOINT TRAJECTORY ------------------------------
+
+%Create sensors mounted on vehicle 
+[paramsVO,imu, mountingLocationIMU,...
+    mountingAnglesIMU] = createSensors(scenario);
 
 
-%SIMULATION SET UP SECOND METHODS ------------------------------------------
-
-% %Set the initial state and error state covariance 
+%Set the initial state and error state covariance 
 filterIntializer(filter,traj,'0');
 
 
-% Run the simulation for 60 seconds.
-numSecondsToSimulate = 60;
+% Run the simulation for 120 seconds.
+numSecondsToSimulate = 120;
 numIMUSamples = numSecondsToSimulate * sampleRate;
 
 % Define the visual odometry sampling rate.
@@ -182,21 +185,30 @@ numCameraSamples = ceil(numIMUSamples / imuSamplesPerCamera);
 RposVO = 0.1;
 RorientVO = 0.1;
 
+% Convert orientation offset from Euler angles to quaternion.
+orientVeh2IMU = quaternion(mountingAnglesIMU,'eulerd','ZYX','frame');
+
 %SIMULATION LOOP
 cameraIdx = 1;
 for i = 1:numIMUSamples
     % Generate ground truth trajectory values.
     [pos(i,:), orient(i,:), vel(i,:), acc(i,:), angvel(i,:)] = traj();
 
+
+    % Convert motion quantities from vehicle frame to IMU frame.
+    [posIMU,orientIMU,velIMU,accIMU,angvelIMU] = transformMotion( ...
+        mountingLocationIMU,orientVeh2IMU, ...
+        pos(i,:),orient(i),vel(i,:),acc(i,:),angvel(i,:));
+
     % Generate accelerometer and gyroscope measurements from the 
     % ground truth trajectory values.
-    [accelMeas, gyroMeas] = imu(acc(i,:), angvel(i,:), orient(i));
+    [accelMeas, gyroMeas] = imu(accIMU,angvelIMU,orientIMU);
 
     % Predict the filter state forward one time step based on the
     % accelerometer and gyroscope measurements.
     predict(filter, accelMeas, gyroMeas); 
 
-    if (1 == mod(i, imuSamplesPerCamera)) && useVIO
+    if (1 == mod(i, imuSamplesPerCamera)) 
         % Generate a visual odometry pose estimate from the ground truth
         % values and the visual odometry model.
         [posVO(cameraIdx,:), orientVO(cameraIdx,:), paramsVO] = ...
@@ -225,19 +237,14 @@ end
 
 %PLOT RESULTS
 figure
-if useVIO
-    plot3(pos(:,1), pos(:,2), pos(:,3), '-.', ...
-        posVO(:,1), posVO(:,2), posVO(:,3), ...
-        posEst(:,1), posEst(:,2), posEst(:,3), ...
-        'LineWidth', 3)
-    legend('Ground Truth', 'Visual Odometry (VO)', ...
-        'Visual-Inertial Odometry (VIO)', 'Location', 'northeast')
-else
-    plot3(pos(:,1), pos(:,2), pos(:,3), '-.', ...
-        posEst(:,1), posEst(:,2), posEst(:,3), ...
-        'LineWidth', 3)
-    legend('Ground Truth', 'IMU Pose Estimate')
-end
+
+plot3(pos(:,1), pos(:,2), pos(:,3), '-.', ...
+    posVO(:,1), posVO(:,2), posVO(:,3), ...
+    posEst(:,1), posEst(:,2), posEst(:,3), ...
+    'LineWidth', 3)
+legend('Ground Truth', 'Visual Odometry (VO)', ...
+    'Visual-Inertial Odometry (VIO)', 'Location', 'northeast')
+
 view(-90, 90)
 title('Vehicle Position')
 xlabel('X (m)')
